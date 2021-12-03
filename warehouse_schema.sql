e0f3d6e9526da98d536c64558168508d9b0cc91e
alexandremr01 <alexandremr01@gmail.com>
2021-12-02 23:06:17 -0300

    add database

--- warehouse_schema.sql
+++ warehouse_schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tempo (
+CREATE TABLE IF NOT EXISTS tempo (
     tempo_key SERIAL PRIMARY KEY,
     mes_numero INT,
     mes_nome VARCHAR(255),
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS lugar (
     numero_logradouro integer
 );
 
-CREATE TABLE pessoa AS (
+CREATE TABLE IF NOT EXISTS pessoa (
     pessoa_key SERIAL PRIMARY KEY,
     tipo_envolvimento VARCHAR,
     sexo CHAR,
@@ -24,30 +24,30 @@ CREATE TABLE pessoa AS (
     grau_instrucao VARCHAR(100)
 );
 
-CREATE TABLE delegacia (
+CREATE TABLE IF NOT EXISTS delegacia (
     delegacia_key SERIAL PRIMARY KEY,
     nome_departamento varchar(255),
     nome_seccional varchar(255),
     nome_delegacia varchar(255)
 );
 
-CREATE TABLE ocorrencias (
-    tempo_ocorrencia_key INTEGER FOREIGN KEY (fk_ocorrencias_tempo_ocorrencia_key) REFERENCES tempo (tempo_key),
-    tempo_registro_key INTEGER FOREIGN KEY (fk_ocorrencias_tempo_registro_key) REFERENCES tempo (tempo_key),
-    delegacia_ocorrencia_key INTEGER FOREIGN KEY (fk_ocorrencias_delegacia_ocorrencia_key) REFERENCES delegacia (delegacia_key),
-    delegacia_registro_key INTEGER FOREIGN KEY (fk_ocorrencias_delegacia_registro_key) REFERENCES delegacia (delegacia_key),
-    lugar_key INTEGER FOREIGN KEY (fk_ocorrencias_lugar_key) REFERENCES lugar (lugar_key),
-    pessoa_key INTEGER FOREIGN KEY (fk_ocorrencias_pessoa_key) REFERENCES pessoa (pessoa_key),
+CREATE TABLE IF NOT EXISTS ocorrencias (
+    tempo_ocorrencia_key INTEGER REFERENCES tempo (tempo_key),
+    tempo_registro_key INTEGER REFERENCES tempo (tempo_key),
+    delegacia_ocorrencia_key INTEGER REFERENCES delegacia (delegacia_key),
+    delegacia_registro_key INTEGER REFERENCES delegacia (delegacia_key),
+    lugar_key INTEGER REFERENCES lugar (lugar_key),
+    pessoa_key INTEGER REFERENCES pessoa (pessoa_key),
 
     num_bo VARCHAR(20),
     flag_status VARCHAR(20),
     rubrica VARCHAR(256),
     conduta VARCHAR(256),
-    hora_ocorencia VARCHAR(8),
+    hora_ocorencia VARCHAR(8)
 );
 
-CREATE TABLE ocorrencias_agregada (
-    tempo_ocorrencia_key INTEGER FOREIGN KEY (fk_ocorrencias_agregada_tempo_ocorrencia_key) REFERENCES tempo (tempo_key),
+CREATE TABLE IF NOT EXISTS ocorrencias_agregada (
+    tempo_ocorrencia_key INTEGER REFERENCES tempo (tempo_key),
     flag_status VARCHAR(20),
     rubrica VARCHAR(256),
     conduta VARCHAR(256),
