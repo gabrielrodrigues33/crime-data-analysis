bf2cf6c8c6a66c413123fbaa3b2f2d18c5d279ac
vasconcelosdavi <davi.muniz41@gmail.com>
2021-12-08 19:41:23 -0300

    Add metrics

--- src/analysis/exploratory.R
+++ src/analysis/exploratory.R
@@ -19,15 +19,19 @@ pessoa <- dbGetQuery(connect, "select * from pessoa")
 # age histogram
 pessoa_idade_quantile <- na.omit(pessoa) %>%
   summarize(lower=quantile(idade, probs=0.025),
-            upper=quantile(idade, probs=0.975))
+            upper=quantile(idade, probs=0.975),
+            mean=mean(idade),
+            median=median(idade))
 
 na.omit(pessoa) %>%
-  filter( idade <= 100) %>% 
+  filter(idade <= 100) %>% 
     ggplot(aes(x=idade), fill="skyblue3") +
     geom_histogram(binwidth=1, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
     labs(y="Número boletins de ocorrência", x="Idade") +
     geom_vline(data=pessoa_idade_quantile, aes(xintercept=lower), linetype="dashed", col="red") +
-    geom_vline(data=pessoa_idade_quantile, aes(xintercept=upper), linetype="dashed", col="red")
+    geom_vline(data=pessoa_idade_quantile, aes(xintercept=upper), linetype="dashed", col="red") +
+    geom_vline(data=pessoa_idade_quantile, aes(xintercept=mean), linetype="dashed", col="darkblue") +
+    geom_vline(data=pessoa_idade_quantile, aes(xintercept=median), linetype="dashed", col="yellow4")
 ggsave("figures/age_hist.png")
   
 # sex histogram
@@ -37,3 +41,11 @@ ggplot(as.data.frame(table(pessoa$sexo))[-2,], aes(x=Var1, y=Freq, fill=Var1)) +
   labs(y="Número boletins de ocorrência", x="Sexo") +
   theme(legend.position="none")
 ggsave("figures/sex_hist.png")
+
+# cor histogram
+na.omit(pessoa) %>%
+  filter(cor != "NULL" & cor != "Vermelha" & cor != "Outros") %>%
+    ggplot(aes(x=cor), fill='skyblue3', stat="count") +
+    geom_bar(fill="skyblue3") +
+    labs(y="Número de boletins de ocorrência", x="Cor")
+ggsave("figures/cor_hist.png")
