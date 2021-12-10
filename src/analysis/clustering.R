4456de32604db9cb476ac1ecca82148357abd980
alexandremr01 <alexandremr01@gmail.com>
2021-12-10 19:47:56 -0300

    add kproto

--- src/analysis/clustering.R
+++ src/analysis/clustering.R
@@ -1,30 +1,42 @@
 # source("src/ports/get_db_remote.R")
 source("src/ports/get_db_local.R")
 con <- dbConnect(RPostgres::Postgres(), dbname = db.name, host=db.host, port=db.port, user=db.user, password=db.password)  
+library(clustMixType)
 
-df <- dbGetQuery(con, 'SELECT * FROM ocorrencias_agregada')
+install.packages(c("cluster", "factoextra"))
+library(cluster)
+library(factoextra)
+library('fpc')
+library(caret)
 
-kmodes_df <- df %>% select(flag_status, rubrica, conduta, tipo_envolvimento)
-x <- kmodes(kmodes_df, modes=3)
+df <- dbGetQuery(con, "
+  SELECT latitude, longitude, rubrica_reduzida, 
+  CASE WHEN hora_ocorrencia<> 'NULL' THEN SUBSTRING(hora_ocorrencia, 1, 2)::INTEGER ELSE NULL END AS hora_ocorrencia 
+  FROM ocorrencias
+  JOIN lugar  ON ocorrencias.lugar_key  = lugar.lugar_key
+")
+df$latitude <- as.numeric(df$latitude)
+df$longitude <- as.numeric(df$longitude)
+df <- na.omit(df)
+
+
+processor <- preProcess(df[,c("latitude", "longitude")], method=c("range"))
+df_processed <- predict(processor, df[,c("latitude", "longitude")])
+df_processed$rubrica_reduzida <- as.factor(df$rubrica_reduzida)
 
 set.seed(123)
 # Compute and plot wss for k = 2 to k = 15.
-k.max <- 15
-data <- kmodes_df
+k.max <- 10
 wss <- sapply(1:k.max, 
-              function(k){sum(kmodes(data, k, iter.max = 15 )$withindiff)})
+              function(k){sum(kproto(df_processed, k, iter.max = 15 )$tot.withinss)})
 wss
 plot(1:k.max, wss,
      type="b", pch = 19, frame = FALSE, 
      xlab="Number of clusters K",
      ylab="Total within-clusters sum of squares")
 
-x <- kmodes(kmodes_df, modes=5)
+x <- kproto(df_processed, k=6)
 x
-kmodes_df$flag_status <- as.factor(kmodes_df$flag_status)
-kmodes_df$rubrica <- as.factor(kmodes_df$rubrica)
-kmodes_df$conduta <- as.factor(kmodes_df$conduta)
-kmodes_df$tipo_envolvimento <- as.factor(kmodes_df$tipo_envolvimento)
 
-kmodes_df$cluster <- as.factor(x$cluster)
-summary(subset(kmodes_df, cluster == 1))
+df_processed$cluster <- as.factor(x$cluster)
+summary(subset(df_processed, cluster == 6))
